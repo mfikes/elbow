b7816e6013fedaabac8b21e6e8e4a3c0d3e4bf1c
Mike Fikes <mike@fikesfarm.com>
2015-11-20 08:59:27 -0500

    Add "out" to clean targets

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
                  [replumb/replumb "0.1.2"]]
+  :clean-targets [:target-path "out"]
   :plugins [[lein-cljsbuild "1.1.1"]]
   :cljsbuild {
     :builds [{
